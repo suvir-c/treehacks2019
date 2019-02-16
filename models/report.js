09e59e04408876817d2d6e2c82285fdc8e3599dc
suvir-c <scoppara@usc.edu>
2019-02-16 13:55:46 -0800

    add encryption for reports

--- models/report.js
+++ models/report.js
@@ -7,32 +7,26 @@ export default class Report extends Model {
         // Offender’s name
         offenderName: {
             type: String,
-            decrypted: true,
         },
         // Date
         date: {
             type: Date,
-            decrypted: true,
         },
         // Location
         location: {
             type: String,
-            decrypted: true,
         },
         // Category of assault
         incidentType: {
             type: String,
-            decrypted: true,
         },
         // Elaboration of case => long paragraph
         longDescription: {
             type: String,
-            decrypted: true,
         },
         // Checkbox to check if ok to talk to police
         authoritiesOK: {
             type: Boolean,
-            decrypted: true,
         },
     }
 }
