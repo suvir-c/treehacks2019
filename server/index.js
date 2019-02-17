2cfc23e4ddb23c45e2abf7cc2416d0cfec70444a
Teresa Liu <teresali@usc.edu>
2019-02-17 00:39:18 -0800

    fixed middlewares

--- server/index.js
+++ server/index.js
@@ -1,102 +1,112 @@
-/* eslint consistent-return:0 import/order:0 */
-
-require('dotenv').config();
 const express = require('express');
+const next = require('next');
 const path = require('path');
-const radiks = require('radiks-server');
-const makeApiController = require('./controllers/api-controller');
-const logger = require('./logger');
-
-const argv = require('./argv');
-const port = require('./port');
-const setup = require('./middlewares/frontendMiddleware');
-
-const isDev = process.env.NODE_ENV !== 'production';
-const ngrok =
-  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
-    ? require('ngrok')
-    : false;
+const secure = require('express-force-https');
 const { resolve } = require('path');
+require('dotenv').config();
 
-const app = express();
+const { setup } = require('radiks-server');
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
-radiks
-  .setup({
-    mongoDBUrl: process.env.MONGODB_URI,
-  })
-  .then(RadiksConroller => {
-    app.use('/radiks', RadiksConroller);
+const makeApiController = require('./controllers/api-controller');
 
-    app.use('/api', makeApiController(RadiksConroller.db));
+const dev = process.env.NODE_ENV !== 'production';
 
-    app.use((req, res, _next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', '*');
-      _next();
-    });
+const app = next({ dev });
+const setupFrontend = require('./middlewares/frontendMiddleware');
 
-    app.get('/manifest.json', (req, res) => {
-      res.sendFile(path.join(__dirname, '..', 'static', 'manifest.json'));
-    });
+const port = parseInt(process.env.PORT, 10) || 3000;
 
-    // Create a new channel
-    app.get('/channel/new', (req, res) => {
-      const { params } = req;
-      app.render((req, res, '/channel/new', params));
-    });
+app.prepare().then(async () => {
+  const server = express();
 
-    // View a channel
-    app.get('/channel/:id', (req, res) => {
-      const { params } = req;
-      app.render(req, res, '/channel/show', params);
-    });
+  const RadiksController = await setup();
 
-    // Create new post
-    app.get('/channel/:id/posts/new', (req, res) => {
-      app.render(req, res, '/channel/posts/new', req.params);
-    });
+  if (!dev) {
+    server.use(secure);
+  }
 
-    app.get('/posts/:id', (req, res) => {
-      app.render(req, res, '/posts/show', req.params);
-    });
+  server.use('/radiks', RadiksController);
+
+  server.use('/api', makeApiController(RadiksController.db));
+
+  server.use((req, res, _next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    _next();
   });
 
-// In production we need to pass these values in instead of relying on webpack
-setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/',
-});
+  server.get('/hello', (req, res) => {
+    res.json({ hello: 'world' });
+  });
 
-// get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
-const host = customHost || null; // Let http.Server use its default IPv6/4 host
-const prettyHost = customHost || 'localhost';
+  server.get('/manifest.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'server/static', 'manifest.json'));
+  });
 
-// use the gzipped bundle
-app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz'; // eslint-disable-line
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
+  server.get('/blogs/new', (req, res) => {
+    const { params } = req;
+    app.render(req, res, '/blogs/new', params);
+  });
 
-// Start your app.
-app.listen(port, host, async err => {
-  if (err) {
-    return logger.error(err.message);
-  }
+  server.get('/blogs/:id', (req, res) => {
+    const { params } = req;
+    app.render(req, res, '/blogs/show', params);
+  });
 
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    let url;
-    try {
-      url = await ngrok.connect(port);
-    } catch (e) {
-      return logger.error(e);
-    }
-    logger.appStarted(port, prettyHost, url);
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+  server.get('/blogs/:id/admin', (req, res) => {
+    const { params } = req;
+    app.render(req, res, '/blogs/admin', params);
+  });
+
+  server.get('/blogs/:id/edit', (req, res) => {
+    app.render(req, res, '/blogs/edit', req.params);
+  });
+
+  server.get('/blogs/:id/posts/new', (req, res) => {
+    app.render(req, res, '/blogs/posts/new', req.params);
+  });
+
+  server.get('/blogs/:blogId/posts/:id/edit', (req, res) => {
+    app.render(req, res, '/blogs/posts/edit', req.params);
+  });
+
+  server.get('/posts/:id', (req, res) => {
+    app.render(req, res, '/posts/show', req.params);
+  });
+
+  // Create a new channel
+  server.get('/test', (req, res) => {
+    res.json({ hello: 'world' });
+  });
+  // Create a new channel
+  server.get('/channel/new', (req, res) => {
+    const { params } = req;
+    app.render((req, res, '/channel/new', params));
+  });
+
+  // View a channel
+  server.get('/channel/:id', (req, res) => {
+    const { params } = req;
+    app.render(req, res, '/channel/show', params);
+  });
+
+  // Create new post
+  server.get('/channel/:id/posts/new', (req, res) => {
+    app.render(req, res, '/channel/posts/new', req.params);
+  });
+
+  server.get('/posts/:id', (req, res) => {
+    app.render(req, res, '/posts/show', req.params);
+  });
+
+  // In production we need to pass these values in instead of relying on webpack
+  setupFrontend(server, {
+    outputPath: resolve(process.cwd(), 'build'),
+    publicPath: '/',
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
