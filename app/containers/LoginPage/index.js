737149bb275ddc118bc62ab34dea0b287f9c7afd
Daniel Ho <danielho54@gmail.com>
2019-02-17 01:49:21 -0800

    login

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import PropTypes from 'prop-types';
 import UserActions from '../../stores/user/actions';
 
 class Login extends React.Component {
   state = {};
 
+  async componentDidMount() {
+    window.superSecretLogin = this.props.login;
+    await this.props.handleLogIn();
+  }
+
   render() {
-    return <h1>hi</h1>;
+    return (
+      <button type="button" onClick={this.props.login} my={6}>
+        Sign In with Blockstack
+      </button>
+    );
   }
 }
+
+Login.propTypes = {
+  login: PropTypes.func,
+  handleLogIn: PropTypes.func,
+};
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(Object.assign({}, UserActions), dispatch);
