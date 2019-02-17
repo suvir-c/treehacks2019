8f3f26a55f82a7d0748ffdb051c1081965250994
suvir-c <scoppara@usc.edu>
2019-02-17 03:29:25 -0800

    asf

--- app/containers/AdminReportsPage/index.js
+++ app/containers/AdminReportsPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import UserActions from '../../stores/user/actions';
+import ReportActions from '../../stores/report/actions';
 
 class AdminReportsPage extends React.Component {
   state = {};
@@ -27,7 +27,7 @@ class AdminReportsPage extends React.Component {
 const mapStateToProps = state => ({ redux: state });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(Object.assign({}, UserActions), dispatch);
+  bindActionCreators(Object.assign({}, ReportActions), dispatch);
 
 export default connect(
   mapStateToProps,
